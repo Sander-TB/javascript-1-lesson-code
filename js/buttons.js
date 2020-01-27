101178658298e5badd9956c17cfcb05a41d7db2d
cnnrbrn <connorbobrien@gmail.com>
2020-01-27 13:25:24 +0200

    update step-8

--- js/buttons.js
+++ js/buttons.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".btn.btn-secondary");
 
 for(let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", loadGames);
+    buttons[i].addEventListener("click", mimicLoadTime);
     buttons[i].addEventListener("click", handleActiveClass);
 }
 
