15283423f0b2725ed32d1e5bb0dff04b1b594c49
cnnrbrn <connorbobrien@gmail.com>
2020-02-03 02:14:17 +0200

    update step-14

--- js/script.js
+++ js/script.js
@@ -34,3 +34,28 @@ function loadGames(json) {
 
     container.innerHTML = newHTML;
 }
+
+function makeGenres(genreArray) {
+    let genreHTML = "";
+
+    genreArray.forEach(function(genre) {
+        let activeClass = "";
+
+        if (genres === genre.slug) {
+            activeClass = "active";
+        }
+        genreHTML += `<a class="genre ${activeClass}" href="index.html?genres=${genre.slug}">${genre.name}</a>`;
+    });
+
+    return genreHTML;
+}
+
+function makePlatforms(platformsArray) {
+    let platformsHTML = "";
+
+    for (let i = 0; i < platformsArray.length; i++) {
+        platformsHTML += `<span>${platformsArray[i].platform.name}</span>`;
+    }
+
+    return platformsHTML;
+}
