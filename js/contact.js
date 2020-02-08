a3ef28d4398f1344c97db15c350f558b7552471a
cnnrbrn <connorbobrien@gmail.com>
2020-02-08 23:32:43 +0200

    update step-19

--- js/contact.js
+++ js/contact.js
@@ -5,6 +5,16 @@ form.addEventListener("submit", validateForm);
 function validateForm(event) {
     event.preventDefault();
     console.log("The form was submitted");
+
+    const firstName = document.querySelector("#firstName");
+    const firstNameError = document.querySelector("#firstNameError");
+    const firstNameValue = firstName.value;
+
+    if (checkInputLength(firstNameValue) === true) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
 }
 
 function checkInputLength(value) {
