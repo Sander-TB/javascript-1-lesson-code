f66a5cb3a3cf5e80c08ad82e0a72868e532e4f40
cnnrbrn <connorbobrien@gmail.com>
2020-02-09 16:40:21 +0200

    update step-21

---
+++ js/creators.js
@@ -0,0 +1,42 @@
+const creatorsUrl = "https://api.rawg.io/api/creators?page=2";
+
+fetch(creatorsUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        handleJson(json);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+function handleJson(json) {
+    const results = json.results;
+    console.dir(results);
+
+    const resultsContainer = document.querySelector(".row.results");
+
+    let html = "";
+
+    results.forEach(function(result) {
+        let imageUrl = "https://via.placeholder.com/250";
+
+        if (result.image) {
+            imageUrl = result.image;
+        }
+
+        html += `<div class="col-sm-6 col-md-4 col-lg-3">
+                    <div class="card">
+                        <div class="image creator" style="background-image: url('${imageUrl}');"></div>
+                        <div class="details">
+                            <h4 class="name">${result.name}</h4>
+                            <p>Game count: ${result.games_count}</p>
+                            <a class="btn details" href="creator.html?id=${result.id}">Details</a>
+                        </div>
+                    </div>
+                </div>`;
+    });
+
+    resultsContainer.innerHTML = html;
+}
